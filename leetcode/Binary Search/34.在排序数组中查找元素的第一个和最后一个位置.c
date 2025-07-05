3d18e163ccc9d87693ca81d7dae7f981eb192e3c
Frayless <15109002620@163.com>
2025-07-05 20:30:41 +0800

    Update 34.在排序数组中查找元素的第一个和最后一个位置.c

--- leetcode/Binary Search/34.在排序数组中查找元素的第一个和最后一个位置.c
+++ leetcode/Binary Search/34.在排序数组中查找元素的第一个和最后一个位置.c
@@ -11,6 +11,8 @@ class Solution {
     }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+        //时间复杂度：O(log n)
+        //空间复杂度：O(1)
         int start = lower_bound(nums, target);
         //对于空数组{}，必须先判断start == nums.size(),用该条件捕获空数组或越界索引，如果先判断后一个条件，会由于数组越界而报错（Segmentation Fault）
         //短路行为：a || b, a true, 不判断b; a && b, a false, 不判断b;
